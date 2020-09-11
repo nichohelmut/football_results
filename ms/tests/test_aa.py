dfeb1ac129ec037f34c20cb35a1435380fcb2179
Nicholas Utikal <nutikal@ambidexter.gmbh>
2020-09-11 17:57:30 +0200

    adds unit tests for archetype analysis

---
+++ ms/tests/test_aa.py
@@ -0,0 +1,46 @@
+from ms.aa import AA
+import numpy as np
+from ms.clustering.clustering import ArchetypalAnalysis
+
+archetype = AA()
+archetype_model = ArchetypalAnalysis(n_archetypes=5, iterations=3, tmax=300)
+
+
+def test_european_leagues():
+    df = archetype.european_leagues()
+
+    '''['Germany', 'France', 'Spain', 'England', 'Italy'] should be included in df'''
+    l_countrie = ['Germany', 'France', 'Spain', 'England', 'Italy']
+    assert True == set(l_countrie).issubset(df['country'].unique())
+
+    '''length of df should 98'''
+    assert len(df) == 98
+
+    '''number of columns of df should 280'''
+    assert len(df.columns) == 280
+
+
+def test_climbers():
+    df = archetype.climbers()
+
+    '''length of df should 7'''
+    assert len(df) == 7
+
+    '''number of columns of df should 280'''
+    assert len(df.columns) == 280
+
+
+def test_matrix():
+    """number of columns of df should 280"""
+    assert type(archetype.matrix()) is np.ndarray
+
+
+def test_archetypal_transform():
+    model = archetype_model.fit(archetype.matrix())
+    A = model.transform(archetype.matrix())
+
+    '''A shpuld be an np.darray'''
+    assert type(A) is np.ndarray
+
+    '''length of A should be number of archetypes selected in model fitting'''
+    assert len(A) == archetype_model.n_archetypes
